9b9b13b6a6869e7bece1bdd4a0fe7fcc40e4ddeb
Feng Shang <newuser@machinezone.com>
2016-04-04 00:29:32 -0700

    upgrade mongoose to v4.4.11

--- server/config/config.js
+++ server/config/config.js
@@ -3,12 +3,13 @@ rootPath = path.normalize(__dirname + '/../../');
 
 module.exports = {
   development: {
-      db: 'mongodb://localhost/spaboilerplate',
+//  db: 'mongodb://localhost/spaboilerplate',
+    db: 'mongodb://fshang:e42jmvml@ds041140.mlab.com:41140/spaboilerplate',
     rootPath: rootPath,
     port: process.env.PORT || 3030
   },
   production: {
-    db: 'mongodb://fshang:e42jmvml@ds041140.mongolab.com:41140/spaboilerplate',
+    db: 'mongodb://fshang:e42jmvml@ds041140.mlab.com:41140/spaboilerplate',
     rootPath: rootPath,
     port: process.env.PORT || 80
   }
